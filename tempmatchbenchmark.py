3e3f94b5789ca2c42084ce4988db8c2656026d25
Yalnyra <vinokur.eg@gmail.com>
2025-11-24 00:54:21 +0200

    visualize keypoint matches

--- tempmatchbenchmark.py
+++ tempmatchbenchmark.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(message)s')
 
 
-def get_norm(self, desc_name):
+def get_norm(desc_name):
     # Binary descriptors need HAMMING, Floats need L2
     if desc_name in ['ORB', 'BRISK', 'AKAZE', 'BRIEF']:
         return cv2.NORM_HAMMING
@@ -161,8 +161,91 @@ if __name__ == "__main__":
                     results.append([det_name, desc_name, len(kp_small), len(good_matches), duration, status])
                     continue
 
-                status = "Success"
-            
+                status = "Success"   
+                matchesMask = mask.ravel().tolist()
+
+                # Make it affine
+                M[2,2] = 1.0
+                M[2,0] = 0.0
+                M[2,1] = 0.0
+
+                # Calculate the rectangle enclosing the query image
+                h,w = template_img.shape
+
+                # Define the rectangle in the coordinates of the template image
+                pts = np.float32([[0,0],[0,h-1],[w-1,h-1],[w-1,0]]).reshape(-1,1,2)
+
+                # transform the rectangle from the template "coordinates" to the query "coordinates"
+                dst = cv2.perspectiveTransform(pts,M)
+
+                if args.photocopied:
+                    logger.info('Simplifying transformation matrix ...'.format(template_img_path))
+                    # if the image is a photocopy or scanned we can assume that there is no shear in x or y.
+                    # Thus we can simplify the transformation matrix M with only: rotation, scale and tranlation.
+
+                    # calculate template "world" reference vectors
+                    w_v = np.array([w-1,0])
+                    h_v = np.array([h-1,0])
+
+                    # calculate query "world" reference vectors
+                    w_vp = (dst[3]-dst[0])[0]
+                    h_vp = (dst[1]-dst[0])[0]
+
+                    # We lost the angle and the scale given that scalation shares the same position in M with the shear transformation
+                    # see https://upload.wikimedia.org/wikipedia/commons/2/2c/2D_affine_transformation_matrix.svg
+                    
+                    # estimate the angle using the top-horizontal line
+                    angle = angle_horizontal(w_vp)
+                    
+                    # estimate the scale using the top-horizontal line and left-vertical line
+                    scale_x = np.linalg.norm(w_vp) / np.linalg.norm(w_v)
+                    scale_y = np.linalg.norm(h_vp) / np.linalg.norm(h_v)
+
+                    # retrieve translation from original matrix M
+                    M = np.matrix([[ scale_x * np.cos(angle) , np.sin(angle)           , M[0,2] ],
+                                    [ -np.sin(angle)          , scale_y * np.cos(angle) , M[1,2] ],
+                                    [ 0                       , 0                       , 1.     ]])
+
+                    # retransform the rectangle with the new matrix
+                    dst = cv2.perspectiveTransform(pts,M)
+                
+                # if bounding boxes are provided and we only have one template
+                # crop those bounding boxes
+                bn, ext = os.path.splitext(os.path.basename(template_img_path))
+                # using M^{-1} we go from query coordinates to template coordinates.
+                img_templ_coords = cv2.warpPerspective(query_img, np.linalg.inv(M), (w,h))
+                logger.info('Saving full image in {}/{}_fix{}'.format(args.output_path, bn, ext))
+                cv2.imwrite('{}/{}_fix{}'.format(args.output_path, bn, ext), img_templ_coords)
+
+                if args.view_matches:
+                    # draw the rectangle in the image
+                    out = cv2.polylines(query_img,[np.int32(dst)],True,0,2, cv2.LINE_AA)
+                    # show the matching features
+                    params = dict(matchColor = (0,255,0), # draw matches in green color
+                                    singlePointColor = None,
+                                    matchesMask = matchesMask, # draw only inliers
+                                    flags = 2)
+                    ## draw the matches image 
+                    out = cv2.drawMatches(template_img, kp_small,
+                                            query_img, kp_big,
+                                            good_matches, 
+                                            None, **params)
+                    ## show result
+                    # --- START EDIT ---
+                    # Calculate the dimensions of the combined image (template + query)
+                    h_graph, w_graph = out.shape[:2]
+                    
+                    # Standard screen DPI (Dots Per Inch) is usually 96. 
+                    # We convert pixels to inches for Matplotlib: inches = pixels / dpi
+                    dpi = 96 
+                    
+                    # Create a figure that matches the pixel resolution of the output image
+                    plt.figure(figsize=(w_graph / dpi, h_graph / dpi), dpi=dpi)
+                    # --- END EDIT ---
+                    ## show result
+                    plt.imshow(out, 'gray')
+                    plt.savefig('{}/{}_match_{}_{}{}'.format(args.output_path, bn, det_name, desc_name, ext), img_templ_coords)
+                    plt.show()
                 
                 end_time = time.time()
                 duration = round(end_time - start_time, 4)
